ed75d4e9b579d6db4035df490d76bfbf2f23e0eb
Virgil Dupras <hsoft@hardcoded.net>
2014-12-27 11:02:47 -0500

    Make the app run under Qt with the updated hscommon
    I expected more work...
    
    Cocoa to come.

--- core/app.py
+++ core/app.py
@@ -1,15 +1,14 @@
 # Created By: Virgil Dupras
 # Created On: 2011-06-12
 # Copyright 2013 Hardcoded Software (http://www.hardcoded.net)
-# 
-# This software is licensed under the "GPL v3" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+#
+# This software is licensed under the "GPL v3" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.hardcoded.net/licenses/gplv3_license
 
 from xml.etree import ElementTree as ET
 from pdfminer.pdfparser import PDFSyntaxError
 
-from hscommon.reg import RegistrableApplication
 from hscommon.notify import Broadcaster
 from hscommon.geometry import Rect
 from hscommon.trans import tr
@@ -38,11 +37,11 @@ class App(Broadcaster):
     # start_job(j, *args)
     # query_load_path(prompt, allowed_exts) --> str_path
     # query_save_path(prompt, allowed_exts) --> str_path
-    
+
     PROMPT_NAME = __appname__
     NAME = PROMPT_NAME
     DEMO_LIMITATION = "will only be able load the 10 first pages of a PDF"
-    
+
     def __init__(self, view):
         Broadcaster.__init__(self)
         self.view = view
@@ -52,13 +51,13 @@ class App(Broadcaster):
         self.pages = []
         self.elements = []
         self.last_file_was_invalid = False
-        
+
         self.element_table = ElementTable(self)
         self.opened_file_label = OpenedFileLabel(self)
         self.page_controller = PageController(self)
         self.build_pane = BuildPane(self)
         self.edit_pane = EditPane(self)
-    
+
     #--- Protected
     def _job_completed(self, jobid):
         # Must be called by subclasses when they detect that an async job is completed.
@@ -69,21 +68,21 @@ class App(Broadcaster):
                 self.notify('elements_changed')
             else:
                 self.view.show_message("This file is not a PDF.")
-    
+
     #--- Public (Internal)
     def select_elements(self, elements):
         if elements == self.select_elements:
             return
         self.selected_elements = elements
         self.notify('elements_selected')
-    
+
     #--- Public (API)
     def open_path(self, path):
         self.view.open_path(path)
-    
+
     def reveal_path(self, path):
         self.view.reveal_path(path)
-    
+
     def change_state_of_selected(self, newstate):
         for element in self.selected_elements:
             if newstate == ElementState.Title:
@@ -95,12 +94,12 @@ class App(Broadcaster):
                     element.title_level = 1
             element.state = newstate
         self.notify('elements_changed')
-    
+
     def load_pdf(self):
         path = self.view.query_load_path("Select a PDF to work with", ['pdf'])
         if not path:
             return
-        
+
         def do(j):
             self.last_file_was_invalid = False
             try:
@@ -108,19 +107,19 @@ class App(Broadcaster):
                 self.current_path = path
             except PDFSyntaxError:
                 self.last_file_was_invalid = True
-        
+
         self.view.start_job(JobType.LoadPDF, do)
-    
+
     def load_project(self):
         path = self.view.query_load_path("Select a PdfMasher project to load", ['masherproj'])
         if not path:
             return
-        
+
         def str2rect(s):
             elems = s.split(' ')
             assert len(elems) == 4
             return Rect(*map(float, elems))
-        
+
         root = ET.parse(path).getroot()
         self.pages = []
         for page_elem in root.iter('page'):
@@ -144,15 +143,15 @@ class App(Broadcaster):
         self.notify('file_opened')
         self.opened_file_label.refresh()
         self.notify('elements_changed')
-        
+
     def save_project(self):
         path = self.view.query_save_path("Select a PdfMasher project to save to", ['masherproj'])
         if not path:
             return
-        
+
         def rect2str(r):
             return "{} {} {} {}".format(*r)
-        
+
         root = ET.Element('pdfmasher-project')
         root.set('pdfpath', self.current_path)
         for page in self.pages:
@@ -171,14 +170,14 @@ class App(Broadcaster):
         tree = ET.ElementTree(root)
         with open(path, 'wb') as fp:
             tree.write(fp, encoding='utf-8')
-    
+
     #--- Properties
     @property
     def hide_ignored(self):
         return self._hide_ignored
-    
+
     @hide_ignored.setter
     def hide_ignored(self, value):
         self._hide_ignored = value
         self.notify('elements_changed')
-    
+
