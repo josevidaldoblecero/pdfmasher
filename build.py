8dab58db7c5e14b62aea75e2671b1c01ae8d2f57
Virgil Dupras <hsoft@hardcoded.net>
2014-12-27 11:37:37 -0500

    Fix OS X build

--- build.py
+++ build.py
@@ -2,9 +2,9 @@
 # Created By: Virgil Dupras
 # Created On: 2011-06-18
 # Copyright 2013 Hardcoded Software (http://www.hardcoded.net)
-# 
-# This software is licensed under the "GPL v3" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+#
+# This software is licensed under the "GPL v3" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.hardcoded.net/licenses/gplv3_license
 
 import sys
@@ -48,10 +48,10 @@ def build_cocoa(dev):
     app = OSXAppStructure('build/PdfMasher.app')
     print('Generating Info.plist')
     app_version = get_module_version('core')
-    filereplace('cocoa/InfoTemplate.plist', 'cocoa/Info.plist', version=app_version)    
+    filereplace('cocoa/InfoTemplate.plist', 'cocoa/Info.plist', version=app_version)
     app.create('cocoa/Info.plist')
     print("Building the cocoa layer")
-    build_cocoalib_xibless(withfairware=False)
+    build_cocoalib_xibless()
     build_xibless()
     pydep_folder = op.join(app.resources, 'py')
     if not op.exists(pydep_folder):
